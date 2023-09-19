b2b1e26ecfd67bd4686ed70c89bb04b0c109c353
Cauan Victor <cauanvictor0325@gmail.com>
2023-09-19 16:17:48 -0300

    feat: environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
-
-// GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
   const transactions = await knex('transactions')
@@ -15,8 +13,8 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3000,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('Server running on port 3000')
+    console.log('Server running on port 3333')
   })
