327846b367a0555ebbb08402c2161bf34972c4ec
Cauan Victor <cauanvictor0325@gmail.com>
2023-09-19 18:00:05 -0300

    feat: create transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,25 @@
+import { randomUUID } from "crypto"
 import { FastifyInstance } from "fastify"
 import knex from "knex"
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-    .select('*')
-    
-    return transactions
+  app.post('/', async (request, reply) => {
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number().positive(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
